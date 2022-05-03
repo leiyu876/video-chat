d5b4510d8d1a46445b4a2345a771928ddf5ee359
Leo Nuneza <leiyu876@yahoo.com>
2022-05-03 22:55:22 +0800

    Add test one vue page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+window.Vue = Vue;
+
+Vue.component('users-component', require('./components/UsersComponent').default);
+
+const app = new Vue({
+    el: '#app'
+});
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/vue-users', function () {
+    return view('vues.users');
+});
